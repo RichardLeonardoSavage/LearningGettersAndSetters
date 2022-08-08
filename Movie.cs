fe548a47ad45451a20beff94035e826aa7d7949c
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-08 17:49:54 +1200

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningGettersAndSetters
+{
+
+
+    //so here is the class that we have assigned properties to
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+
+
+        //this is the constructor 
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+
+            
+        }
+
+
+        //here we have defined a method to determine that the Rating property can only have certain values
+        public string Rating
+        {
+            
+            get { return rating; }
+            set {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR") 
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningGettersAndSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+
+
+            //this won't register because we have set it so that the Rating property is private and can only be a value that we have approved even if we try to hard code a different rating in
+            shrek.Rating = "dog";
+
+
+            Console.WriteLine(shrek.Rating);
+            Console.ReadLine();
+        }
+    }
+}
